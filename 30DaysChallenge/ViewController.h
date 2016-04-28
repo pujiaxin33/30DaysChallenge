ada79dc74bb67b038716fb5e1af3b0d776ce993e
pujiaxin33 <www.317437084@qq.com>
2016-04-28 10:31:49 +0800

    Initial Commit

---
+++ 30DaysChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  30DaysChallenge
+//
+//  Created by jiaxin on 16/4/28.
+//  Copyright © 2016年 jiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 30DaysChallenge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  30DaysChallenge
+//
+//  Created by jiaxin on 16/4/28.
+//  Copyright © 2016年 jiaxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
